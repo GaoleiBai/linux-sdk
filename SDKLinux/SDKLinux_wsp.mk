e9d478f4455f6427061751ecfca6d45c0cd8001e
iso9660 <daniel.m.v@terra.es>
2012-11-27 13:14:24 -0800

    Non esential files are removed

--- SDKLinux/SDKLinux_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ SDKLinux - Debug ]----------
-	@"$(MAKE)" -f "SDKLinux.mk"
-clean:
-	@echo ----------Cleaning project:[ SDKLinux - Debug ]----------
-	@"$(MAKE)" -f "SDKLinux.mk" clean
