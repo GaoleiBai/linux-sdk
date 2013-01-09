3d57a652d9b5268c41b015ccc3dbe233caea0506
iso9660 <daniel.m.v@terra.es>
2013-01-09 05:56:19 -0800

    Worked in Dictionary

--- SDKLinux/LFC/Collections/dictionary.h
+++ SDKLinux/LFC/Collections/dictionary.h
@@ -2,6 +2,7 @@
 #define DICTIONARY_H
 
 #include "../n_object.h"
+#include <stdlib.h>
 
 template<class K, class V>
 class DictionaryEntry {
@@ -27,7 +28,10 @@ public:
 	Dictionary(int (*COMPARER)(const void *u, const void *v));
 	virtual ~Dictionary();
 	
-	void AddEntry(K key, V value);
+	void SetEntry(K key, V value);
+	void DeleteEntry(K key);
+	
+	V &operator[](K key);
 	
 private:
 	DictionaryEntry<K, V> *entries;
@@ -37,6 +41,8 @@ private:
 	static int (*KEY_COMPARER)(const void *u, const void *v);
 	
 	void ensureCapacity(int capacity);
+	void qsort();
+	int binarySearchIx(K key);
 	static int ENTRY_COMPARER(const void *u, const void *v);
 
 };
@@ -49,6 +55,12 @@ Dictionary<K, V>::Dictionary(int (*COMPARER)(const void *u, const void *v))
 	isSorted = false;
 	KEY_COMPARER = COMPARER;
 	ensureCapacity(1000);
+}
+
+template<class K, class V>
+Dictionary<K, V>::~Dictionary()
+{
+	delete entries;
 }
 
 template<class K, class V>
@@ -66,6 +78,25 @@ void Dictionary<K, V>::ensureCapacity(int capacity)
 }
 
 template<class K, class V>
+void Dictionary<K, V>::qsort()
+{
+	if (isSorted) return;
+	::qsort(entries, numEntries, sizeof(entries[0]), ENTRY_COMPARER);
+	isSorted = true;
+}
+
+template<class K, class V>
+int Dictionary<K, V>::binarySearchIx(K key)
+{
+	qsort();
+	DictionaryEntry<K, V> *e = new DictionaryEntry<K, V>[1];
+	void *q = bsearch(&e, entries, numEntries, sizeof(entries[0]), ENTRY_COMPARER);
+	delete e;
+	if (q == NULL) return -1;
+	return ((unsigned long)q - (unsigned long)entries) / sizeof(entries[0]);
+}
+
+template<class K, class V>
 int Dictionary<K, V>::ENTRY_COMPARER(const void *u, const void *v)
 {
 	DictionaryEntry<K, V> **uu = (DictionaryEntry<K, V> **)u;
@@ -78,11 +109,27 @@ int Dictionary<K, V>::ENTRY_COMPARER(const void *u, const void *v)
 }
 
 template<class K, class V>
-void Dictionary<K, V>::AddEntry(K key, V value)
+void Dictionary<K, V>::SetEntry(K key, V value)
 {
-	ensureCapacity(numEntries + 1);
-	entries[numEntries++] = new DictionaryEntry<K, V>(key, value);
-	isSorted = false;
+	int ix = binarySearchIx(key);
+	if (ix != -1) {
+		entries[ix]->Value = value;
+	} else {
+		aaa
+		ensureCapacity(numEntries + 1);
+		entries[numEntries++] = new DictionaryEntry<K, V>(key, value);
+		isSorted = false;
+	}
+}
+
+template<class K, class V>
+void Dictionary<K, V>::DeleteEntry(K key)
+{
+	int ix = binarySearchIx(key);
+	if (ix == -1) return;
+	numEntries--;
+	for (int i=ix; i<numEntries; i++)
+		entries[i] = entries[i + 1];
 }
 
 #endif // DICTIONARY_H
