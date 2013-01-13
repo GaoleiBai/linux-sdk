cb24f5ffea9f38841b56ac1fa309e07e9617ee66
iso9660 <daniel.m.v@terra.es>
2013-01-13 14:44:42 -0800

    Added attributes to helpclass

--- SDKLinux/Tests/SerializationHelpClasses/helpclass.h
+++ SDKLinux/Tests/SerializationHelpClasses/helpclass.h
@@ -14,8 +14,25 @@ public:
 	
 private:
 	char nchar;
+	short nshort;
 	int nint;
-
+	long nlong;
+	long long nlonglong;
+	unsigned char nuchar;
+	unsigned short nushort;
+	unsigned int nuint;
+	unsigned long nulong;
+	unsigned long long nulonglong;
+	float nfloat;
+	double ndouble;
+	long double nlongdouble;
+	Text *atext;
+	DateTime *adatetime;
+	NObjectCollection *nobjectcollection;
+	NObjectDictionary *nobjectdictionary;
+	Buffer *abuffer;
+	TextBuffer *atextbuffer;
+	
 };
 
 #endif // HELPCLASS_H
