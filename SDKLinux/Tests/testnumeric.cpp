83361261f970164669035830837021a97d75500b
iso9660 <daniel.m.v@terra.es>
2013-01-17 12:00:54 -0800

    Added TestNumeric

---
+++ SDKLinux/Tests/testnumeric.cpp
@@ -0,0 +1,20 @@
+#include "testnumeric.h"
+#include "../LFC/LFC.h"
+
+TestNumeric::TestNumeric()
+{
+}
+
+TestNumeric::~TestNumeric()
+{
+}
+
+int TestNumeric::Perform()
+{
+	NBool b = false;
+	NChar c = 'c';
+	
+	NInt i = 0;
+	return 0;
+}
+
