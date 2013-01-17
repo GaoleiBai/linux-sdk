e3aa272c865cd687cb486d78ccba0355fdf39543
iso9660 <daniel.m.v@terra.es>
2013-01-17 12:06:35 -0800

    Project updated

--- SDKLinux/Tests/testnumeric.h
+++ SDKLinux/Tests/testnumeric.h
@@ -1,20 +1,33 @@
-#include "testnumeric.h"
-#include "../LFC/LFC.h"
+/* Copyright (C) 2012, 2013
+   Daniel Mosquera Villanueva (daniel.m.v@terra.es)
+   This file is part of LFC Library.
 
-TestNumeric::TestNumeric()
-{
-}
+   LFC Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-TestNumeric::~TestNumeric()
-{
-}
+   LFC Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-int TestNumeric::Perform()
-{
-	NBool b = false;
-	NChar c = 'c';
-	
-	NInt i = 0;
-	return 0;
-}
+   You should have received a copy of the GNU Lesser General Public
+   License along with LFC Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA. or see http://www.gnu.org/licenses/. */
+   
+   
+#ifndef TESTNUMERIC_H
+#define TESTNUMERIC_H
 
+class TestNumeric {
+
+public:
+	TestNumeric();
+	virtual ~TestNumeric();
+
+	static int Perform();
+};
+
+#endif // TESTNUMERIC_H
