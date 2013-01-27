cb4af1d7eec88a6fcb2c4ed8ba831b15d24fca52
iso9660 <daniel.m.v@terra.es>
2013-01-27 13:23:29 -0800

    Added MathException

--- SDKLinux/LFC/Math/math.cpp
+++ SDKLinux/LFC/Math/math.cpp
@@ -1,4 +1,5 @@
 #include "math.h"
+
 #include <math.h>
 
 Math::Math()
@@ -67,4 +68,64 @@ double Math::CosH(double r)
 double Math::TanH(double r)
 {
 	return tanh(r);
+}
+
+double Math::Exp(double n)
+{
+	return exp(n);
+}
+
+void Math::GetSigExp(double n, double &significant, int &exponent)
+{
+	significant = frexp(n, &exponent);
+}
+
+double Math::FromSigExp(double significant, int exponent)
+{
+	return ldexp(significant, exponent);
+}
+
+double Math::Log(double n)
+{
+	return log(n);
+}
+
+double Math::Log10(double n)
+{
+	return log10(n);
+}
+
+void Math::GetIntFrac(double n, double &integer_part, double &fractional_part)
+{
+	fractional_part = modf(n, &integer_part);
+}
+
+double Math::Pow(double x, double y)
+{
+	return pow(x, y);
+}
+
+double Math::Sqrt(double n)
+{
+	return sqrt(n);
+}
+
+double Math::Ceil(double n)
+{
+	return ceil(n);
+}
+
+double Math::Floor(double n)
+{
+	return floor(n);
+}
+
+double Math::Abs(double n)
+{
+	return fabs(n);
+}
+
+double Math::Mod(double numerator, double denominator)
+{
+	return fmod(numerator, denominator);
 }
--- SDKLinux/LFC/Math/math.h
+++ SDKLinux/LFC/Math/math.h
@@ -20,6 +20,18 @@ public:
 	static double SinH(double r);
 	static double CosH(double r);
 	static double TanH(double r);
+	static double Exp(double n);
+	static void GetSigExp(double n, double &significant, int &exponent);
+	static double FromSigExp(double significant, int exponent);
+	static double Log(double n);
+	static double Log10(double n);
+	static void GetIntFrac(double n, double &integer_part, double &fractional_part);
+	static double Pow(double x, double y);
+	static double Sqrt(double n);
+	static double Ceil(double n);
+	static double Floor(double n);
+	static double Abs(double n);
+	static double Mod(double numerator, double denominator);
 
 };
 
