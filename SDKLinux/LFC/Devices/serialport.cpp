d9258462e29c47808faf02f0da6a3b39e36ba93b
iso9660 <daniel.m.v@terra.es>
2013-01-30 13:27:52 -0800

    Added some functions to serial port

--- SDKLinux/LFC/Devices/serialport.cpp
+++ SDKLinux/LFC/Devices/serialport.cpp
@@ -1,10 +1,29 @@
 #include "serialport.h"
+#include "../Text/text.h"
 
-SerialPort::SerialPort()
+SerialPort::SerialPort(const Text &deviceName)
 {
+	portDeviceName = new Text(deviceName);
+	f = -1;
 }
 
 SerialPort::~SerialPort()
 {
+	Close();
+	delete portDeviceName;
 }
 
+Text SerialPort::ToText()
+{
+	return *portDeviceName;
+}
+
+void SerialPort::Open()
+{
+	
+}
+
+void SerialPort::Close()
+{
+	
+}
--- SDKLinux/LFC/Devices/serialport.h
+++ SDKLinux/LFC/Devices/serialport.h
@@ -1,11 +1,27 @@
 #ifndef SERIALPORT_H
 #define SERIALPORT_H
 
-class SerialPort {
+#include "../n_object.h"
+#include "../FileSystem/ifile.h"
+
+class SerialPort : public NObject, public IFile {
 
 public:
-	SerialPort();
+	SerialPort(const Text &deviceName);
 	virtual ~SerialPort();
+	
+	void Open();
+	void Close();
+
+	Text ToText();
+
+private:
+	int f;
+	Text *portDeviceName;
+	int portSpeed;
+	int portDataBits;
+	int portParity;
+	int portStopBits;
 
 };
 
