a1b7921d8b8ea26fdc4c68f815b4cd5cc2eabfca
iso9660 <daniel.m.v@terra.es>
2013-01-30 13:50:54 -0800

    Added code to open function

--- SDKLinux/LFC/Devices/serialport.cpp
+++ SDKLinux/LFC/Devices/serialport.cpp
@@ -1,5 +1,11 @@
 #include "serialport.h"
+#include "deviceexception.h"
 #include "../Text/text.h"
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 SerialPort::SerialPort(const Text &deviceName)
 {
@@ -20,10 +26,28 @@ Text SerialPort::ToText()
 
 void SerialPort::Open()
 {
-	//if (fd != -1) throw 
+	if (fd != -1) return;
+	
+	char cadena[1001];
+	portDeviceName->GetAnsiString(cadena, 1000);
+	
+	// Se abre el dispositivo
+	errno = 0;
+	fd = open(cadena, O_RDWR | O_NOCTTY | O_NDELAY ); 
+	if (fd == -1) throw new DeviceException(Text::FromErrno(), __FILE__, __LINE__, __func__);
+	
+	// Se configura el puerto serie para no bloquearse si no hay datos en el buffer de entrada
+	if (fcntl(fd, F_SETFL, FNDELAY) == -1) {
+		close(fd);
+		fd = -1;
+		throw new DeviceException(Text::FromErrno(), __FILE__, __LINE__, __func__);
+	}
 }
 
 void SerialPort::Close()
 {
-	
+	if (fd == -1) return;
+	errno = 0;
+	if (close(fd) == -1) 
+		throw new DeviceException(Text::FromErrno(), __FILE__, __LINE__, __func__);
 }
